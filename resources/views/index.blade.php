f9fed9d9aa669bd398c14205a9843e0792040406
TrixXii <bellatrixX738@gmail.com>
2023-03-19 19:22:40 +0100

    kok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
         <div class="container">
             <h1>Lista de libros</h1>
             <hr>
+
             <form method="GET" action="{{ route('books') }}">
         <div class="form-group">
             <label for="category_id">Filtrar por categoría:</label>
@@ -16,6 +17,8 @@
             </select>
         </div>
     </form> 
+    <a class="m-3" href="{{ route('create') }}">Crear</a>
+
     <div id="contenido" class="row row-cols-md-3 g-3 m-auto">
     @foreach($books as $book)
     <div class="col">
@@ -25,46 +28,24 @@
                 <h5 class="card-title">{{$book->title}}</h5>
                 <p class="card-text"><b>Author:</b>{{$book->author}}</p>
             </div>
-            <a href="{{ route('edit', $book->id) }}">Editar</a>
-            <form method="POST" action="{{ route('destroy', ['id' => $book->id]) }}">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Eliminar</button>
-                </form>
-                
-                <a href="{{ route('show', $book->id) }}">Mas info</a>
+    
             <div class="pb-2 card-footer">
                 <small class="text-muted "><b>isbn:</b>{{$book->isbn}}</small>
+                <div class="d-flex p-1">
+                    <a href="{{ route('edit', $book->id) }}" class="btn btn-warning m-1">Editar</a>
+                    <form method="POST" action="{{ route('destroy', ['id' => $book->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger m-1">Eliminar</button>
+                    </form>
+                    
+                    <a href="{{ route('show', $book->id) }}" class="btn btn-info m-1">Mas info</a>
+                </div>
             </div>
-            
         </div>
     </div>
     @endforeach</div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">isbn</th>
-                        <th scope="col">Titulo</th>
-                        <th scope="col">Author</th>
-                        <th scope="col">Editar</th>
-                        <th scope="col">Eliminar</th>
-                        <th scope="col">Ver mas</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($books as $book)
-                        <tr>
-                            <th scope="row">{{$book->isbn}}</th>
-                            <td>{{$book->title}}</td>
-                            <td>{{$book->author}}</td>
-                            <td><a href="{{ route('edit', $book->id) }}">Editar</a></td>
-                            
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
 
-            <a class="m-3" href="{{ route('create') }}">Crear</a>
 
            <p class="m-3"> {{$books->links()}}</p>
         </div>
