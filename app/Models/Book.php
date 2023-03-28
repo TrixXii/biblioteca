b557bcacd498321f4c041f98f3c347a6cae5c37d
TrixXii <bellatrixX738@gmail.com>
2023-03-28 19:23:34 +0200

    erer

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -13,3 +13,38 @@ class Book extends Model
         return $this->belongsToMany(Category::class, 'book_category', 'book_id', 'category_id');
     }
 }
+
+// namespace App;
+
+// use Illuminate\Database\Eloquent\Model;
+
+// class Event extends Model
+// {
+//     public function user()
+//     {
+//         return $this->belongsTo(User::class);
+//     }
+// }
+
+// namespace App\Http\Controllers;
+
+// use App\Event;
+
+// class EventController extends Controller
+// {
+//     public function index()
+//     {
+//         $events = Event::with('user')->get();
+
+//         return view('index', compact('events'));
+//     }
+// }
+
+// @foreach ($events as $event)
+//     <div>
+//         <h2>{{ $event->title }}</h2>
+//         <p>Location: {{ $event->location }}</p>
+//         <p>Date: {{ $event->date }}</p>
+//         <p>Created by: {{ $event->user->name }}</p>
+//     </div>
+// @endforeach
